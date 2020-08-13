b7a376f400485c42907db629bd07d9e66c6e14bd
lordlinda <marialindananono@gmail.com>
2020-08-13 11:16:04 +0300

    heroku

---
+++ client/build/precache-manifest.cc53681ff9771798c13cfb97c1c7f7aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2bd43fd931c049a6c3feae4f693cbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7c3a73103fc373d03f2",
+    "url": "/static/css/main.211d9502.chunk.css"
+  },
+  {
+    "revision": "a2419ca85db9e47151a8",
+    "url": "/static/js/2.d28b9d05.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.d28b9d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7c3a73103fc373d03f2",
+    "url": "/static/js/main.be0c3136.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
